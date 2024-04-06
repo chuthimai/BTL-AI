7243e92fd81bc801ff6225af20b03292a4bf5147
maichu <chuthimai18245@gmail.com>
2024-04-07 00:27:08 +0700

    add dfs

---
+++ algorithms/dfs.py
@@ -0,0 +1,39 @@
+from models.point import Point, print_route
+
+matrix_example = [
+    [0, 1, 1, 1, 0, 1, 0, 0, 0],
+    [0, 0, 0, 0, 1, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 1, 0, 0],
+    [0, 0, 1, 0, 0, 0, 1, 1, 0],
+    [0, 0, 0, 0, 0, 1, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0]
+]
+
+
+def dfs_algorithms(matrix, start=0, end=8):
+    set_of_bound = []
+    visited = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+    route = []
+    open_point = Point(start)
+    while True:
+        route.insert(0, open_point)
+        visited[open_point.name] = 1
+        for i in range(8, -1, -1):
+            if visited[i] == 0 and matrix[open_point.name][i] > 0:
+                p = Point(name=i, before=open_point)
+                set_of_bound.insert(0, p)
+        open_point = set_of_bound[0]
+        set_of_bound.pop(0)
+        if open_point.name == end:
+            route.insert(0, open_point)
+            break
+
+    return route
+
+
+print_route(dfs_algorithms(matrix_example))
+
+
--- models/point.py
+++ models/point.py
@@ -1,8 +1,22 @@
+ref = {0: "S", 1: "A", 2: "B", 3: "C", 4: "D", 5: "E", 6: "F", 7: "H", 8: "G"}
+
+
 class Point:
-    def __init__(self, name, h=0):
+    def __init__(self, name, h=0, before=None):
         self.name = name
         self.h = h
-        self.before = None
+        self.before = before
 
     def set_before_point(self, point: 'Point'):
         self.before = point
+
+    def __repr__(self):
+        if self.before is None:
+            return f"{ref[self.name]}(Null)"
+        return f"{ref[self.name]}({ref[self.before.name]})"
+
+
+def print_route(route):
+    for point in route[:-1]:
+        print(ref[point.name], end="<-")
+    print(ref[route[-1].name])
